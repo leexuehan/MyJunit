5c40a915a613c7452627e53bd24b54318fb2d184
lixuehan <xuehanlee@163.com>
2017-09-13 22:18:13 +0800

    多测试用例执行 失败打印原因

--- lxh-junit/src/main/java/origin/CalculatorTest.java
+++ lxh-junit/src/main/java/origin/CalculatorTest.java
@@ -4,17 +4,24 @@ package origin;
  * Developed by Lee Happily.
  */
 public class CalculatorTest {
-    public void testCalculator() throws Exception {
+    public void testCalculatorShouldFail() throws Exception {
         Calculator calculator = new Calculator();
         int result = calculator.add(1, 2);
         shouldEquals(0, result);
     }
 
+    public void testCalculatorShouldSuccess() throws Exception {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(1, 2);
+        shouldEquals(3, result);
+    }
+
+
     private void shouldEquals(int expect, int actual) throws Exception {
         if (expect != actual) {
-            throw new Exception("not equals");
-        }else {
-            System.out.println("pass test");
+            System.out.println("Test Failed: actual is " + actual + ", expect is: " + expect);
+        } else {
+            System.out.println("Test Passed");
         }
     }
 
